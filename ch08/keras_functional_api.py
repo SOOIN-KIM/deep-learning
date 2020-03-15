a8407562b528691bbc3944b88af2a5809cf2dcd9
Sooin Kim <59334939+analyst-sooin@users.noreply.github.com>
2020-03-15 20:45:22 +0900

    Add files via upload

---
+++ ch08/keras_functional_api.py
@@ -0,0 +1,54 @@
+'''
+
+Keras Function API
+'''
+import numpy as np
+
+from tensorflow.keras import Sequential, Model
+from tensorflow.keras.layers import Dense
+from tensorflow.keras import Input
+
+# X(N,64) -> Dense(32) -> ReLU -> Dense(32) -> ReLU -> Dense(10) -> Softmax
+
+seq_model = Sequential()
+seq_model.add(Dense(32,activation='relu', input_shape=(64,)))
+seq_model.add(Dense(32,activation='relu'))
+seq_model.add(Dense(10,activation='softmax'))
+seq_model.summary()
+
+print()
+# Keras의 함수형 API 기능을 사용해서 신경망 생성하는 방법
+# Input 객체생성
+# -> 필요한 레이어 객체 생성 & 인스턴스 호출
+# -> Model 객체를 생성
+input_tensor = Input(shape=(64,)) # 입력 텐서의 sahpe를 결정
+print(type(input_tensor))
+
+# 첫번째 은닉층(hidden layer) 생성 & 인스턴스 호출을 사용해서 입력 데이터 전달
+# x = Dense(32,activation='relu')(input_tensor)
+dense1 = Dense(32,activation='relu')
+x = dense1(input_tensor)
+print(type(x))
+
+# 두번째 은닉층 생성 & 첫번쨰 은닉층의 출력을 입력으로 전달
+x = Dense(32,activation='relu')(x)
+
+# 출력층 생성 & 두번쨰 은닉층의 출력을 입력으로 전달
+output_tensor = Dense(10,activation='softmax')(x)
+
+# 신경망 모델 생성 -input/out 텐서를 Model 생성자의 파라미터에 전달
+model = Model(input_tensor, output_tensor)
+
+# 모델 요약 정보
+model.summary()
+
+# 모델 생성 후
+# -> 모델 컴파일 compile -> 모델 학습(fit) -> 모델 평가(evaluate), 예측(predict)
+
+model.compile(optimizer='adam', loss = 'categorical_crossentropy')
+x_train = np.random.random(size=(1000,64))
+y_train = np.random.randint(10,size=(1000, 10))
+model.fit(x_train, y_train,epochs=10, batch_size=128)
+
+score = model.evaluate(x_train,y_train)
+print(score)
